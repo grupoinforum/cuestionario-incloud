491dc0a5463a437eeade7f871eed19915560b518
grupoinforum <info@inforumsol.com>
2025-10-16 11:17:27 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,23 +26,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }}
         />
 
-        {/* === Meta Pixel Code === */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1744327902933881');
-            fbq('track', 'PageView');`,
-          }}
-        />
+        {/* === Meta Pixel Code (sin duplicado) === */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            if (!window.fbq) {
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1744327902933881');
+              fbq('track', 'PageView');
+            }
+          `}
+        </Script>
         <noscript>
           <img
             height="1"
