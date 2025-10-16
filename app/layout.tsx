4adb7b2c9e03d513671bbfd20b8a0d9ee55e37cf
grupoinforum <info@inforumsol.com>
2025-10-16 10:28:46 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,32 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             })(window, document, "clarity", "script", "tpqh51e1s5");`,
           }}
         />
+
+        {/* === Meta Pixel Code === */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1744327902933881');
+            fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1744327902933881&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
 
       <body className="min-h-screen bg-white text-gray-900 antialiased flex flex-col">
@@ -50,8 +76,48 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <footer className="w-full bg-[#f5f5f5] py-4 text-center text-sm text-gray-500">
           © {new Date().getFullYear()} Inforum
         </footer>
+
+        {/* === LinkedIn Insight Tag === */}
+        <Script
+          id="linkedin-insight"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "8112330";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `,
+          }}
+        />
+        <Script
+          id="linkedin-insight-loader"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(l) {
+                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8112330&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
 }
+
 
