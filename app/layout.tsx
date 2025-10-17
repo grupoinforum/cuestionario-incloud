cc75ab042e2555d0d6393e03961228ff1e55767c
grupoinforum <info@inforumsol.com>
2025-10-17 16:53:02 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,33 +79,35 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         {/* === LinkedIn Insight Tag === */}
         <Script
-          id="linkedin-insight"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              _linkedin_partner_id = "8112330";
-              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-            `,
-          }}
-        />
-        <Script
-          id="linkedin-insight-loader"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(l) {
-                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-                window.lintrk.q=[]}
-                var s = document.getElementsByTagName("script")[0];
-                var b = document.createElement("script");
-                b.type = "text/javascript";b.async = true;
-                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                s.parentNode.insertBefore(b, s);
-              })(window.lintrk);
-            `,
-          }}
-        />
+  id="linkedin-insight"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      var _linkedin_partner_id = "8112330";
+      window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+      window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+    `,
+  }}
+/>
+<Script
+  id="linkedin-insight-loader"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(l) {
+        if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]}
+        var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script"); b.type = "text/javascript"; b.async = true;
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+        s.parentNode.insertBefore(b, s);
+      })(window.lintrk);
+    `,
+  }}
+/>
+<noscript>
+  <img height="1" width="1" style={{ display: "none" }} alt=""
+    src="https://px.ads.linkedin.com/collect/?pid=8112330&fmt=gif" />
+</noscript>
         <noscript>
           <img
             height="1"
